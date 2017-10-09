e836f5c53d94ec0b8a73d1e3e0774928bce08d59
adryfab <adry_fab@hotmail.com>
2017-10-09 18:44:57 -0500

    [#10] Enemigo

---
+++ Assets/Scripts/EnemyControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControl : MonoBehaviour {
+
+    public float velocity = 2f;
+
+    private Rigidbody2D rg2d;
+
+	// Use this for initialization
+	void Start () {
+        rg2d = GetComponent<Rigidbody2D>();
+        rg2d.velocity = Vector2.left * velocity;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
